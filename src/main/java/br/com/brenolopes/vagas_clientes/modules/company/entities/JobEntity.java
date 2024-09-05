dcc6cb3ea36f75447a11eb6ade68892c06c862d8
Breno Lopes <brenotolentino.dev@gmail.com>
2024-09-05 18:30:06 -0300

    first commit

--- src/main/java/br/com/brenolopes/vagas_clientes/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/brenolopes/vagas_clientes/modules/company/entities/JobEntity.java
@@ -1,14 +1,18 @@
 package br.com.brenolopes.vagas_clientes.modules.company.entities;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.validator.constraints.UUID;
-import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity(name = "job")
 @Data
@@ -17,10 +21,16 @@ public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String description;
     private String benefits;
     private String level;
 
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false) 
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id", insertable = false, updatable = false)  
     private UUID companyId;
 
     @CreationTimestamp
